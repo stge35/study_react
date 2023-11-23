7237e8ca0d13f6c68b3ccdc0a6ced88d203f8771
stge35 <evanok1@gmail.com>
2023-11-23 11:49:37 +0900

    Add: more complicated component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,11 @@
 function ExpenseItem() {
-    return <h2>Expense Item!</h2>
+    return (
+        <div>
+            <div>March 28th 2023</div>
+            <h2>Car Insurance</h2>
+            <div>$294.67</div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
