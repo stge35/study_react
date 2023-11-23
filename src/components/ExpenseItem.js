5ecb0fbfe81476d3647035b20c2284dcd0d441ad
stge35 <evanok1@gmail.com>
2023-11-23 11:57:50 +0900

    Add: use basic css in component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,13 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
     return (
-        <div>
+        <div className= "expense-item">
             <div>March 28th 2023</div>
-            <h2>Car Insurance</h2>
-            <div>$294.67</div>
+            <div className="expense-item__description">
+                <h2>Car Insurance</h2>
+                <div className="expense-item__price">$294.67</div>
+            </div>
         </div>
     );
 }
