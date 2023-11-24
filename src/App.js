c622b7ee0bc1d92f433ef7a4b8c850e5d4b6cba2
stge35 <evanok1@gmail.com>
2023-11-24 16:16:22 +0900

    Add: Study useStates

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import NewExpense from "./components/NewExpense/NewExpense";
+
 import Expenses from "./components/Expenses/Expenses";
 
 const App = () => {
@@ -24,7 +26,7 @@ const App = () => {
     ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+        <NewExpense />
         <Expenses items={expenses}></Expenses>
     </div>
   );
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,26 @@
+import React, {useState} from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) =>{
+
+    const [title, setTitle] = useState(props.title);
+    // title = 관리되는 해당 값에 대한 포인터
+    // setTitle = 나중에 새 품명을 설정할 때 호출할 함수
+    // 일반적으로 첫 번째 인자를 이용하여 함수 이름을 만든다.
+
     const clickHandler = () =>{
-        console.log('Clicked!!!');
+        setTitle('Updated');
+        console.log(title);
     }
 
     return (
         <Card className= "expense-item">
             <ExpenseDate date={props.date}></ExpenseDate>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
             </div>
             <button onClick={clickHandler}>Change Title</button>
